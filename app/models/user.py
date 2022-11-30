b31b664860321667e9742fa89b88cd4894fe6c4e
tatsuki1107 <tatsuki11070522@icloud.com>
2022-11-30 16:52:17 +0900

    init migration && run seed

---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+from database import Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from sqlalchemy_utils import UUIDType
+from uuid import uuid4
+from .mixins import TimestampMixin
+
+
+class User(Base, TimestampMixin):
+    __tablename__ = "users"
+
+    uuid = Column(
+        UUIDType(binary=False), primary_key=True, default=uuid4
+    )
+
+    username = Column(String(128), nullable=False)
+
+    # relation設定
+    books = relationship("Book", back_populates="user")
